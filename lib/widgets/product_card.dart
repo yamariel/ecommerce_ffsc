c466a770f1fb9e4a6915d1b8b73c164a6fe1655f
yamariel <ariel.yamien@epitech.eu>
2026-08-26 07:37:43 +0000

    [ADD]: Création de la carte des produits

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,63 @@
+import '../models/product.dart';
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  final VoidCallback onTap;
+  const ProductCard({super.key, required this.product, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 3,
+        child: InkWell(
+          onTap: onTap,
+          child: Column(
+            children: [
+              Expanded(
+                child: Hero(
+                  tag: product.id,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
+                      product.image,
+                      width: double.infinity,
+                      errorBuilder: (context, error, stackTrace) {
+                        return SizedBox(
+                          height: 180,
+                          child: Center(
+                            child: Icon(
+                              Icons.image_not_supported_outlined,
+                              size: 50,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+                
+              Text(product.name, style: TextStyle(fontWeight: FontWeight.bold)),
+              const SizedBox(height: 10,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("${product.price}€"),
+                  Row(
+                    children: [
+                      Text("${product.rating}"), 
+                      Icon(Icons.star, color: Colors.yellow,)
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
