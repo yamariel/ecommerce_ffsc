197194b623724c83cdc08cca6278c2e85af53b0f
yamariel <ariel.yamien@epitech.eu>
2026-08-26 16:31:07 +0000

    [UPDATE]: Ajout de la navigation vers la page détail

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/screens/product_detail_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -17,7 +18,14 @@ class ProductListScreen extends ConsumerWidget {
               maxCrossAxisExtent: 300,
             ),
             itemCount: products.length,
-            itemBuilder: (context, i) => ProductCard(product: products[i], onTap: () {}),
+            itemBuilder: (context, i) => ProductCard(product: products[i], onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => ProductDetailScreen(product: products[i]),
+                ),
+              );
+            }),
           ),
           error: (err, stack) => Center(child: Text('Erreur: $err')),
           loading: () => Center(child: const CircularProgressIndicator(color: Colors.blue,)),
