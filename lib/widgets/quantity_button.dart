f217dd69ad1b99220cafd615db6444edb8fa5213
yamariel <ariel.yamien@epitech.eu>
2026-08-29 06:01:53 +0000

    [UPDATE]: Mise à jour de la taille du container dans le constructeur et centrage de l'icone

--- lib/widgets/quantity_button.dart
+++ lib/widgets/quantity_button.dart
@@ -3,20 +3,32 @@ import 'package:flutter/material.dart';
 class QuantityButton extends StatelessWidget {
   final IconData icon;
   final VoidCallback onPressed;
-  const QuantityButton({super.key, required this.onPressed, required this.icon});
+  final double width;
+  final double heigth;
+  const QuantityButton({
+    super.key,
+    required this.onPressed,
+    required this.icon,
+    required this.heigth,
+    required this.width
+  });
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: width,
+      height: heigth,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(12),
         color: Colors.white,
-        border: Border.all(
-          color: Colors.grey.shade500
-        )
+        border: Border.all(color: Colors.grey.shade500),
       ),
-      child: IconButton(
-        onPressed: onPressed, 
-        icon: Icon(icon)
+      child: Center(
+        child: IconButton(
+          onPressed: onPressed,
+          icon: Icon(icon, size: 20),
+          padding: EdgeInsets.zero,
+          constraints: const BoxConstraints(),
+        ),
       ),
     );
   }
